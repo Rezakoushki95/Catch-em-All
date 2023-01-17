b1c7ec48dfcd659c1b7167757940c31b7ae03277
rezakoushki95 <rezaaarhus@hotmail.com>
2023-01-17 03:46:00 +0100

    Make the app compatible with the parsed JSON

---
+++ Catch 'em All/ListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Catch 'em All
+//
+//  Created by Reza Koushki on 12/01/2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	var pokemons = Pokemons()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		print("\(indexPath.row+1) of \(pokemons.pokemonArray.count)")
+		let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+		if indexPath.row == pokemons.pokemonArray.count-1 && pokemons.urlString.hasPrefix("http") {
+			pokemons.getData {
+				DispatchQueue.main.async {
+					self.tableView.reloadData()
+				}
+			}
+		}
+		cell.textLabel?.text = "\(indexPath.row+1). \(pokemons.pokemonArray[indexPath.row].name)"
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return pokemons.pokemonArray.count
+	}
+	
+}
+
---
+++ Catch 'em All/Pokemons.swift
@@ -0,0 +1,58 @@
+//
+//  Pokemons.swift
+//  Catch 'em All
+//
+//  Created by Reza Koushki on 12/01/2023.
+//
+
+import Foundation
+
+class Pokemons {
+	
+	private struct Returned: Codable {
+		var count: Int
+		var next: String?
+		var results: [Pokemon]
+	}
+	
+	struct Pokemon: Codable {
+		var name = ""
+		var url = ""
+	}
+	
+	var count = 0
+	var urlString = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"
+	var pokemonArray: [Pokemon] = []
+	
+	func getData(completed: @escaping ()->()) {
+		print("We are accessing the url \(urlString)")
+		
+		// Create a URL
+		guard let url = URL(string: urlString) else {
+			print("ERROR: Couln't get create a URL from \(urlString)")
+			return
+		}
+		
+		// Create a URLSession
+		let session = URLSession.shared
+		
+		// Get the data with .dataTask method
+		let task = session.dataTask(with: url) { data, response, error in
+			if let error = error {
+				print("ERROR: \(error.localizedDescription)")
+			}
+			do {
+				let returned = try JSONDecoder().decode(Returned.self, from: data!)
+				print("Here is what was returned \(returned)")
+				self.pokemonArray.append(contentsOf: returned.results)
+				self.urlString = returned.next ?? ""
+				self.count = returned.count
+				 
+			} catch {
+				print("JSON ERROR: Thrown when we tried to decode from Returned.self with data")
+			}
+			completed()
+		}
+		task.resume()
+	}
+}
