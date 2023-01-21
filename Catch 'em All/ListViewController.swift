4dba006b3788eedbeec29491de52adc5e3947877
rezakoushki95 <rezaaarhus@hotmail.com>
2023-01-21 20:03:44 +0100

    - Create, configure, show, and remove a UIActivityView when .getData() is called

--- Catch 'em All/ListViewController.swift
+++ Catch 'em All/ListViewController.swift
@@ -12,17 +12,31 @@ class ListViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
 	
 	var pokemons = Pokemons()
+	var activityIndicator = UIActivityIndicatorView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.delegate = self
 		tableView.dataSource = self
+		setUpActivityIndicator()
+		activityIndicator.startAnimating()
+		activityIndicator.isUserInteractionEnabled = false
 		pokemons.getData {
 			DispatchQueue.main.async {
 				self.navigationItem.title = "\(self.pokemons.pokemonArray.count) of \(self.pokemons.count) pokemons."
 				self.tableView.reloadData()
+				self.activityIndicator.stopAnimating()
+				self.activityIndicator.isUserInteractionEnabled = true
 			}
 		}
+	}
+	
+	func setUpActivityIndicator() {
+		activityIndicator.center = self.view.center
+		activityIndicator.hidesWhenStopped = true
+		activityIndicator.style = .large
+		activityIndicator.color = UIColor.red
+		view.addSubview(activityIndicator)
 	}
 	
 	func loadAll() {
@@ -35,11 +49,17 @@ class ListViewController: UIViewController {
 				self.loadAll()
 			}
 		} else {
-			print("All done - all loaded. Total Pokemons = \(pokemons.pokemonArray.count)")
+			DispatchQueue.main.async {
+				print("All done - all loaded. Total Pokemons = \(self.pokemons.pokemonArray.count)")
+				self.activityIndicator.stopAnimating()
+				self.activityIndicator.isUserInteractionEnabled = true
+			}
 		}
 	}
 	
 	@IBAction func loadAllButtonPressed(_ sender: UIBarButtonItem) {
+		activityIndicator.startAnimating()
+		activityIndicator.isUserInteractionEnabled = false
 		loadAll()
 	}
 }
@@ -49,10 +69,14 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
   		let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
 		if indexPath.row == pokemons.pokemonArray.count-1 && pokemons.urlString.hasPrefix("http") {
+			self.activityIndicator.stopAnimating()
+			self.activityIndicator.isUserInteractionEnabled = true
 			pokemons.getData {
 				DispatchQueue.main.async {
 					self.navigationItem.title = "\(self.pokemons.pokemonArray.count) of \(self.pokemons.count) pokemons."
 					self.tableView.reloadData()
+					self.activityIndicator.stopAnimating()
+					self.activityIndicator.isUserInteractionEnabled = true
 				}
 			}
 		}
